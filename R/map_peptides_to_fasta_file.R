717770ae7c75a2c63a8637c4c39e8e6586f60c20
Tyler Sagendorf <57922480+TylerSagendorf@users.noreply.github.com>
2022-02-02 08:27:49 -0800

    Remove roxygen docs
    Remove documentation created by roxygen2 and create separately.

--- R/map_peptides_to_fasta_file.R
+++ R/map_peptides_to_fasta_file.R
@@ -1,37 +1,4 @@
-#' Map peptides to FASTA file
-#'
-#' @description
-#' Takes list of clean peptide sequences and FASTA file and returns a
-#' data frame containing all matches.
-#' 
-#' @param peptides (character) vector of clean peptide sequences.
-#' @param fasta (AAStringSet object)
-#' @param numCores (numeric) number of copies of \R to run in parallel.
-#' 
-#' @return (data.frame) with two columns for peptide and accession
-#' 
-#' @importFrom parallel detectCores makeCluster clusterEvalQ clusterExport 
-#'   clusterSplit clusterApply stopCluster
-#' @importFrom dplyr %>% mutate filter select
-#' 
-#' @author 
-#' Michael Nestor
-#' 
-#' @examples
-#' msnid <- MSnID(".")
-#' mzids <- system.file("extdata", "phospho.mzid.gz", package="MSnID")
-#' msnid <- read_mzIDs(msnid, mzids)
-#' 
-#' peptides <- peptides(msnid)
-#' 
-#' path_to_FASTA <- system.file("extdata", "for_phospho.fasta.gz", 
-#'                              package = "MSnID")
-#' fasta <- Biostrings::readAAStringSet(path_to_FASTA)
-#' 
-#' out <- map_peptides_to_fasta_file(peptides, fasta, numCores = 2L)
 
-
-#' @export
 map_peptides_to_fasta_file <- function(peptides, fasta, 
                                        numCores = detectCores() - 1) 
 {
