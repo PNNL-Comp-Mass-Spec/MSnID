a0c7a74493585169402e6535f1123dcdf71b7618
Tyler Sagendorf <57922480+TylerSagendorf@users.noreply.github.com>
2022-02-01 23:51:34 -0800

    Add map_peptides_to_fasta_file
    Michael added this function, but it was never exported: https://github.com/PNNL-Comp-Mass-Spec/MSnID/commit/5e2f3dd304d5036b80090e8c21426654678b0ffe

--- R/map_peptides_to_fasta_file.R
+++ R/map_peptides_to_fasta_file.R
@@ -4,29 +4,38 @@
 #' Takes list of clean peptide sequences and FASTA file and returns a
 #' data frame containing all matches.
 #' 
-#' @param peptides (character) list of clean peptide sequences
-#' @param fasta (AAStringSet)
+#' @param peptides (character) vector of clean peptide sequences.
+#' @param fst (AAStringSet object)
+#' @param numCores (numeric) number of copies of \R to run in parallel.
+#' 
 #' @return (data.frame) with two columns for peptide and accession
-#' @importFrom parallel detectCores makeCluster clusterEvalQ clusterExport clusterSplit clusterApply stopCluster
+#' 
+#' @importFrom parallel detectCores makeCluster clusterEvalQ clusterExport 
+#'   clusterSplit clusterApply stopCluster
 #' @importFrom dplyr %>% mutate filter select
 #' 
-#' @example 
+#' @author 
+#' Michael Nestor
+#' 
+#' @examples
 #' msnid <- MSnID(".")
-#' mzids <- system.file("extdata","phospho.mzid.gz",package="MSnID")
+#' mzids <- system.file("extdata", "phospho.mzid.gz", package="MSnID")
 #' msnid <- read_mzIDs(msnid, mzids)
 #' 
-#' peptides <- unique(msnid$pepSeq)
+#' peptides <- peptides(msnid)
 #' 
-#' fst_path <- system.file("extdata","for_phospho.fasta.gz",package="MSnID")
-#' library(Biostrings)
-#' fst <- readAAStringSet(fst_path)
+#' path_to_FASTA <- system.file("extdata", "for_phospho.fasta.gz", 
+#'                              package = "MSnID")
+#' fst <- Biostrings::readAAStringSet(path_to_FASTA)
 #' 
 #' out <- map_peptides_to_fasta_file(peptides, fst, numCores = 2L)
 
 
 #' @export
-map_peptides_to_fasta_file <- function(peptides, fasta, numCores = detectCores() - 1) {
-  
+map_peptides_to_fasta_file <- function(peptides, fst, 
+                                       numCores = detectCores() - 1) 
+{
+
   fasta.df <- data.frame(accession = names(fst),
                          sequence = fst,
                          row.names = NULL)
@@ -36,9 +45,9 @@ map_peptides_to_fasta_file <- function(peptides, fasta, numCores = detectCores()
   clusterExport(cl, "peptides")
   
   fasta.df.split <- lapply(clusterSplit(cl, 1:nrow(fasta.df)),
-                          function(idx) {
-                            fasta.df[idx,, drop=FALSE]
-                          }) 
+                           function(idx) {
+                             fasta.df[idx,, drop=FALSE]
+                           }) 
   
   f <- function(peptides, fasta.df) {
     lapply(peptides, function(peptide) {
@@ -47,7 +56,7 @@ map_peptides_to_fasta_file <- function(peptides, fasta, numCores = detectCores()
         filter(is_match) %>%
         mutate(peptide = peptide) %>%
         select(peptide, accession, -sequence, -is_match)
-      })
+    })
   }
   
   out <- clusterApply(cl, fasta.df.split, function(fasta.df) {
@@ -57,3 +66,6 @@ map_peptides_to_fasta_file <- function(peptides, fasta, numCores = detectCores()
   out <- unlist(out, recursive = FALSE)
   out <- do.call(rbind, out)
 }
+
+utils::globalVariables(c("is_match"))
+
