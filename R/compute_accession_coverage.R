e1b6acda24ef817f32ffff78ccd191ec188941ba
Tyler Sagendorf <57922480+TylerSagendorf@users.noreply.github.com>
2022-02-01 23:57:22 -0800

    Update compute_accession_coverage
    Greatly speed up compute_accession_coverage. It now takes ~1 minute to process ~14K accessions.

--- R/compute_accession_coverage.R
+++ R/compute_accession_coverage.R
@@ -1,72 +1,73 @@
 
-get_coverage_for_single_accession <- function(accession_i, ids, fasta, pepSeq_col) {
-  
-  
-  x <- ids %>% 
-    filter(accession == accession_i) %>%
-    distinct()
-  
-  accAAstring <- fasta[[accession_i]]
-  
-  # main loop
-  irl <- IRangesList()
-  for(j in 1:nrow(x)) {
-    pepSeq_j = x[[pepSeq_col]][j]
-    mtch <- regexpr(pepSeq_j, accAAstring)
-    if(mtch != -1){
-       start <- as.numeric(mtch)
-       width <- attr(mtch, "match.length")
-       tgt <- IRanges(start=start, width=width, names=pepSeq_j)
-       irl[[j]] <- tgt
-    }
-  }
-  
-  fullAACoverage <- sum(IRanges::width(reduce(unlist(irl))))
-  percentAACoverage <- 100*fullAACoverage/length(accAAstring)
-  return(percentAACoverage)
-}
-
-
 .compute_accession_coverage <- function(object,
-                                      fasta,
-                                      accession_col,
-                                      pepSeq_col) {
+                                        fasta,
+                                        accession_col,
+                                        pepSeq_col) 
+{
+  # Clean up on exit - optional, but frees up memory
+  on.exit(rm(list = ls()))
+  on.exit(gc(verbose = FALSE), add = TRUE)
   
-  # check for decoy entries
-  decoy_acc <- apply_filter(object, "isDecoy")[[accession_col]]
+  ## MS/MS data.table ----
+  msms_cols <- c(accession_col, pepSeq_col)
+  msms.dt <- unique(object@psms[, msms_cols, with = FALSE])
+  
+  ## Protein sequence data.table ----
+  # Include reverse sequences if there are decoys
+  decoy_acc <- unique(apply_filter(object, "isDecoy")[[accession_col]])
   if (length(decoy_acc) > 0 & !any(decoy_acc %in% names(fasta))) {
     fasta_rev <- reverse(fasta)
     names(fasta_rev) <- paste0("XXX_", names(fasta))
     fasta <- c(fasta, fasta_rev)
   }
-
-  # check if fasta entry names are unique
-  if(any(duplicated(names(fasta)))){
-    message("FASTA entry names are not unique!\n")
-    stop()
-  }
   
-  # check if there is at least some agreement in IDs
-  if (!all(object[[accession_col]] %in% names(fasta))) {
-    stop("Some accession IDs not found in FASTA entries!\n")
-  }
+  # Reduce fasta to relevant accessions and create data.table
+  fasta <- fasta[names(fasta) %in% msms.dt[[accession_col]]]
+  fasta.dt <- data.table(width = width(fasta), 
+                         seq = as.character(fasta),
+                         accession_col = names(fasta))
   
-  ids <- psms(object) %>% 
-    select(accession_col, pepSeq_col) %>%
-    distinct()
+  # Merge MSMS and FASTA data.tables
+  msms.dt <- merge(msms.dt, fasta.dt, 
+                   by.x = accession_col, by.y = "accession_col")
   
-  res <- ids %>% 
-    select(accession_col) %>%
-    distinct()
+  ## Calculate overlap ----
+  # Get starting positions of each peptide in corresponding
+  # protein sequence by accession and calculate width of each peptide
+  msms.dt[, start := as.numeric(stringr::str_locate(pattern = get(pepSeq_col),
+                                                    string = seq)[, 1]),
+          by = accession_col][, peptide_width := nchar(get(pepSeq_col))]
   
-  res$percentAACoverage <-  map(res[[accession_col]],
-                                get_coverage_for_single_accession,
-                                ids, fasta, pepSeq_col)
-  res$percentAACoverage <- as.numeric(res$percentAACoverage)
+  # Create IRanges object to calculate overlap
+  irl <- IRanges(start = msms.dt[, start],
+                 width = msms.dt[, peptide_width],
+                 names = msms.dt[, pepSeq_col, with = FALSE])
+  irl <- split(irl, msms.dt[, accession_col, with = FALSE]) # convert to list
   
-  res <- inner_join(psms(object), res, by=accession_col)
-
-  psms(object) <- res
+  # Calculate AA overlap - this part takes the longest
+  fullAACoverage <- sapply(irl, function(irl_i) {
+    sum(IRanges::width(reduce(irl_i)))
+  })
+  # Convert named list to data.table
+  fullAACoverage.dt <- data.table(fullAACoverage = fullAACoverage,
+                                  accession_col = names(fullAACoverage))
   
+  # Add fullAACoverage column to dt
+  msms.dt <- merge(msms.dt, fullAACoverage.dt,
+                   by.x = accession_col, by.y = "accession_col")
+  # Calculate percentAACoverage and only keep required columns
+  msms.dt <- unique(
+    msms.dt[, percentAACoverage :=
+              100 * fullAACoverage / width][
+                , c(accession_col, "percentAACoverage"), with = FALSE]
+  )
+  
+  # Add percentAACoverage column to psms(object)
+  object@psms <- merge(object@psms, msms.dt, by = accession_col)
   return(object)
 }
+
+utils::globalVariables(c("start", "peptide_width", "percentAACoverage"))
+
+
+
